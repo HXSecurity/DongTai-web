bc4074bc0c433873c6d5980eb25936f054d4e2b1
owefsad <dongzhiyong@secnium.cn>
2020-12-23 16:40:29 +0800

    增加前端权限验证

--- src/router/index.ts
+++ src/router/index.ts
@@ -20,14 +20,22 @@ router.beforeEach((to, from, next) => {
       .dispatch('user/getUserInfo')
       .then(() => {
         Nprogress.done()
-        next()
+        if (store.getters.userInfo.role != 1 && (to.fullPath == '/user/userList' || to.fullPath == '/setting/sysInfo' || to.fullPath == '/setting/upgradeOnline')) {
+          next('/')
+        } else {
+          next()
+        }
       })
       .catch(() => {
         Nprogress.done()
         next('/login')
       })
   } else {
-    next()
+    if (store.getters.userInfo.role != 1 && (to.fullPath == '/user/userList' || to.fullPath == '/setting/sysInfo' || to.fullPath == '/setting/upgradeOnline')) {
+      next('/')
+    } else {
+      next()
+    }
   }
 })
 
