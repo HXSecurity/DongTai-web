de50fe972e86d7f68cf43065d0b785c576164e81
piexlmax <303176530@qq.com>
2022-03-31 19:05:21 +0800

    fixed:agentConfig Auth

--- src/views/setting/SysInfo.vue
+++ src/views/setting/SysInfo.vue
@@ -6,7 +6,7 @@
         name="global"
       ></el-tab-pane>
       <el-tab-pane
-        v-if="userInfo.role === 1 && userInfo.role === 2"
+        v-if="userInfo.role === 1 || userInfo.role === 2"
         :label="$t('views.sysInfo.fuse')"
         name="agent"
       ></el-tab-pane>
